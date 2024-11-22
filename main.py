d83af4ca5296365ed3f1f422eb6aa26de26c752a
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2024-11-22 07:52:01 +0100

    no change, just reformatted

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 from datetime import datetime
+
 from fastapi import FastAPI, HTTPException, Depends
+from fastapi_utils.tasks import repeat_every
 from typing_extensions import Annotated
 from pydantic import BaseModel, Field
 from sqlmodel import Session
-from fastapi_utils.tasks import repeat_every
 import uvicorn
 
-from model import Game, Player, create_new_snake, create_db_and_tables, get_game, get_player, get_session, Direction, move_all_game, engine
+from model import (
+    Game,
+    Player,
+    create_new_snake,
+    create_db_and_tables,
+    get_game,
+    get_player,
+    get_session,
+    Direction,
+    move_all_game,
+    engine,
+)
 
 create_db_and_tables()
 
 app = FastAPI()
+
 
 class GameSchema(BaseModel):
     name: Annotated[str, Field(example="Game Name")]
@@ -19,24 +32,38 @@ class GameSchema(BaseModel):
     class Config:
         from_attributes = True
 
+
 class SnakePositionSchema(BaseModel):
     x: Annotated[int, Field(description="The x coordinate of the snake position")]
     y: Annotated[int, Field(description="The y coordinate of the snake position")]
-    created_at: Annotated[datetime, Field(description="The created at date of the snake position")]
+    created_at: Annotated[
+        datetime, Field(description="The created at date of the snake position")
+    ]
+
 
 class CreatePlayerSchema(BaseModel):
-    direction: Annotated[Direction, Field(example="UP", description="The direction the player is facing", default=Direction.UP)]
+    direction: Annotated[
+        Direction,
+        Field(
+            example="UP",
+            description="The direction the player is facing",
+            default=Direction.UP,
+        ),
+    ]
     name: Annotated[str, Field(example="Player Name")]
     color: Annotated[str, Field(example="#000000")]
 
     class Config:
         from_attributes = True
 
+
 class PlayerSchema(CreatePlayerSchema):
     id: Annotated[int, Field(description="The ID of the player")]
 
+
 class PlayerWithSnakeSchema(PlayerSchema):
     positions: list[SnakePositionSchema] = []
+
 
 class GameWithPlayersSchema(GameSchema):
     players: list[PlayerWithSnakeSchema] = []
@@ -46,20 +73,25 @@ class GameWithPlayersSchema(GameSchema):
 def create_game(game: GameSchema, session: Annotated[Session, Depends(get_session)]):
     if get_game(game.slug, session) is not None:
         raise HTTPException(status_code=400, detail="Game already exists for this slug")
-    
+
     new_game = Game.model_validate(game)
     session.add(new_game)
     session.commit()
     session.refresh(new_game)
     return new_game
 
+
 @app.post("/game/{slug}/register", response_model=GameWithPlayersSchema)
-def register_player(slug: str, player: CreatePlayerSchema, session: Annotated[Session, Depends(get_session)]):
+def register_player(
+    slug: str,
+    player: CreatePlayerSchema,
+    session: Annotated[Session, Depends(get_session)],
+):
     game = get_game(slug, session)
 
     if game is None:
         raise HTTPException(status_code=404, detail="Game not found")
-    
+
     player_data = player.model_dump()
     player_data["game_id"] = game.id
     new_player = Player.model_validate(player_data)
@@ -71,28 +103,34 @@ def register_player(slug: str, player: CreatePlayerSchema, session: Annotated[Se
     session.refresh(new_player)
     return new_player.game
 
+
 @app.get("/game/{slug}", response_model=GameWithPlayersSchema)
 def show_game(slug: str, session: Annotated[Session, Depends(get_session)]):
     game = get_game(slug, session)
 
     if game is None:
         raise HTTPException(status_code=404, detail="Game not found")
-    
+
     return game
 
+
 @app.patch("/player/{id}", response_model=PlayerSchema)
-def update_player(id: int, direction: Direction, session: Annotated[Session, Depends(get_session)]):
+def update_player(
+    id: int, direction: Direction, session: Annotated[Session, Depends(get_session)]
+):
     player = get_player(id, session)
 
     if player is None:
         raise HTTPException(status_code=404, detail="Player not found")
-    
+
     player.direction = direction
     session.add(player)
     session.commit()
     session.refresh(player)
     return player
 
+
+# note that on_event seems to be a deprecated feature
 @app.on_event("startup")
 @repeat_every(seconds=5, raise_exceptions=True)
 def tick() -> None:
@@ -101,5 +139,6 @@ def tick() -> None:
         move_all_game(session)
         session.commit()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     uvicorn.run(app)
--- model.py
+++ model.py
@@ -1,21 +1,24 @@
 from datetime import datetime
 import random
 import enum
+from collections import Counter
 from typing import Optional
+
 from sqlalchemy import Column, Enum
 from sqlmodel import Field, SQLModel, Session, create_engine, Relationship, select
-from collections import Counter
 
 X_RANGE = 100
 Y_RANGE = 100
 PADDING = 10
 SNAKE_INITIAL_LENGTH = 2
 
+
 class Direction(str, enum.Enum):
     UP = "UP"
     DOWN = "DOWN"
     LEFT = "LEFT"
     RIGHT = "RIGHT"
+
 
 # Define the models first
 class Game(SQLModel, table=True):
@@ -24,6 +27,7 @@ class Game(SQLModel, table=True):
     slug: str = Field(index=True, unique=True)
     players: list["Player"] = Relationship(back_populates="game")
 
+
 class Player(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     direction: Direction = Field(sa_column=Column(Enum(Direction)))
@@ -31,8 +35,12 @@ class Player(SQLModel, table=True):
     game_id: int = Field(foreign_key="game.id")
     game: Game = Relationship(back_populates="players")
     color: str = Field(index=True)
-    positions: list["SnakePosition"] = Relationship(back_populates="player", sa_relationship_kwargs={"order_by": "desc(SnakePosition.created_at)"})
-    
+    positions: list["SnakePosition"] = Relationship(
+        back_populates="player",
+        sa_relationship_kwargs={"order_by": "desc(SnakePosition.created_at)"},
+    )
+
+
 class SnakePosition(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     player_id: int = Field(foreign_key="player.id")
@@ -52,6 +60,7 @@ engine = create_engine(sqlite_url, connect_args=connect_args)
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
+
 def get_session():
     db = Session(bind=engine)
     try:
@@ -59,9 +68,11 @@ def get_session():
     finally:
         db.close()
 
+
 def get_game(slug: str, session: Session) -> Game | None:
     return session.exec(select(Game).where(Game.slug == slug)).one_or_none()
-        
+
+
 def get_player(id: int, session: Session) -> Player | None:
     return session.exec(select(Player).where(Player.id == id)).one_or_none()
 
@@ -78,6 +89,7 @@ def move_position(x: int, y: int, direction: Direction) -> tuple[int, int]:
 
     return x, y
 
+
 def create_new_snake(player: Player, session: Session):
     x = random.randint(PADDING, X_RANGE - PADDING)
     y = random.randint(PADDING, Y_RANGE - PADDING)
@@ -90,20 +102,27 @@ def create_new_snake(player: Player, session: Session):
 
     session.add_all(positions)
 
+
 def move_all_game(session: Session):
     games = session.exec(select(Game)).all()
     for game in games:
         remove_old_positions(game, session)
         generate_new_positions(game, session)
 
+
 def remove_old_positions(game: Game, session: Session):
-    """ 
-    Remove the oldest position for each player as long as it has more than 1 positions
+    """
+    Remove the oldest position for each player as long as it has more than 1 position
     """
     for player in game.players:
-        positions = session.exec(select(SnakePosition).where(SnakePosition.player_id == player.id).order_by(SnakePosition.created_at.desc())).all()
+        positions = session.exec(
+            select(SnakePosition)
+            .where(SnakePosition.player_id == player.id)
+            .order_by(SnakePosition.created_at.desc())
+        ).all()
         if len(positions) > 1:
             session.delete(positions[-1])
+
 
 def generate_new_positions(game: Game, session: Session):
     """
@@ -111,20 +130,25 @@ def generate_new_positions(game: Game, session: Session):
     """
     new_positions = []
     for player in game.players:
-        x, y = move_position(player.positions[0].x, player.positions[0].y, player.direction)
+        x, y = move_position(
+            player.positions[0].x, player.positions[0].y, player.direction
+        )
         new_positions.append(SnakePosition(x=x, y=y, player=player))
-    
+
     # Remove duplicate positions based on x,y coordinates
     position_by_coords = Counter()
     for position in new_positions:
         pos_tuple = (position.x, position.y)
         position_by_coords[pos_tuple] += 1
 
-    new_positions = [position for position in new_positions if position_by_coords[pos_tuple] == 1]
+    new_positions = [
+        position for position in new_positions if position_by_coords[pos_tuple] == 1
+    ]
 
     for position in new_positions:
         if can_create_position(game, position, session):
             session.add(position)
+
 
 def can_create_position(game: Game, position: SnakePosition, session: Session):
     """
@@ -134,12 +158,20 @@ def can_create_position(game: Game, position: SnakePosition, session: Session):
     # Check if the position is within the bounds of the game
     if not 0 < position.x < X_RANGE or not 0 < position.y < Y_RANGE:
         return False
-    
+
     # Check if the position is already occupied
-    if session.exec(select(SnakePosition).where(SnakePosition.x == position.x, SnakePosition.y == position.y)).first() is not None:
+    if (
+        session.exec(
+            select(SnakePosition).where(
+                SnakePosition.x == position.x, SnakePosition.y == position.y
+            )
+        ).first()
+        is not None
+    ):
         return False
 
     return True
+
 
 if __name__ == "__main__":
     create_db_and_tables()
